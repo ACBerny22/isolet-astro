65256526a360fd798e078e6d30cd5321c2ed6080
mau2212 <mau221201@gmail.com>
2024-01-29 14:06:42 -0600

    proto-done

--- src/components/BarMenu.jsx
+++ src/components/BarMenu.jsx
@@ -7,8 +7,28 @@ export default function BarMenu() {
     const $isMenuOpen = useStore(isMenuOpen);
     console.log($isMenuOpen)
 
-    return $isMenuOpen ? 
-    <aside className='absolute top-0 bg-zinc-900'>...</aside> 
-    : 
-    null;
+
+    return(
+        <>  
+        {
+        $isMenuOpen && 
+        <div className="fixed top-0 right-0 h-screen w-screen bg-zinc-800 p-20 animated-div z-40 overflow-y-auto" aria-labelledby="dropdownDefaultButton">
+            <div className='flex justify-center items-center bg-zinc-800 w-full text-white p-5'>
+                <div className='flex justify-center items-center flex-col gap-10'>
+                    <div class='w-48'>
+                        <img src="/isolet.png" alt="" />
+                    </div>
+                    <h1 className='text-4xl font-light'>Isolet Salon</h1>
+                    <div class="items-center gap-10 font-light uppercase text-sm tracking-wide flex flex-col z-50">
+                        <a href="#servicios" class=" transition hover:bg-white/30  p-3  rounded" onClick={() => isMenuOpen.set(!$isMenuOpen)}>Servicios</a>
+                        <a href="#about" class=" transition p-3 hover:bg-white/30   rounded" onClick={() => isMenuOpen.set(!$isMenuOpen)}>Conocenos</a>
+                        <a href="#galeria" class=" transition p-3 hover:bg-white/30  rounded" onClick={() => isMenuOpen.set(!$isMenuOpen)}>Galeria</a>
+                        <a href="#contacto" class="transition p-3 hover:bg-white/30   rounded" onClick={() => isMenuOpen.set(!$isMenuOpen)}>Contacto</a>
+                    </div>
+                </div>   
+            </div>
+        </div>
+        }      
+        </>
+    )
 }
